bf8ee35a56b4236acfd9171cb5d6a3af9a134e92
Brandon Cox <bcox@calderaengineering.com>
2020-08-26 08:45:12 -0600

    finished alphaCarryoverCalc

--- alphaCarryoverCalc.py
+++ alphaCarryoverCalc.py
@@ -28,12 +28,25 @@ def getValuesFromString(string,v):
 
 def getFloatFromString(string):
     words = string.split(' ')
-    num = float(words[1][:-1])
+    num = float(words[1].strip(';\n'))
     return num
+
+def getDfromString(string):
+    words = string.split(' ')
+    xlow = float(words[3].strip('('))
+    ylow = float(words[4])
+    zlow = float(words[5].strip(')'))
+    xhigh = float(words[6].strip('('))
+    yhigh = float(words[7])
+    zhigh = float(words[8].strip(')\n'))
+    diffV = np.array([xhigh-xlow,yhigh-ylow,zhigh-zlow])
+    D = max(diffV)
+    return D
 
 def getArgs():
     parser.add_argument('--v', '--OpenFOAM Version',nargs='*',type=str,default=['ofo'])
     parser.add_argument('--w', '--Rolling Mean Window',nargs='*',type=int,default=[5000])
+    parser.add_argument('--l', '--Case Label',nargs='*',type=str,default=['none'])
     parser.add_argument('--case1', '--Case 1 Path',nargs='*',type=str,default=['none'])
     parser.add_argument('--v1', '--OpenFOAM Version for case 1',nargs='*',type=str,default=['ofo'])
     parser.add_argument('--case2', '--Case 2 Path',nargs='*',type=str,default=['none'])
@@ -48,6 +61,7 @@ def getArgs():
     args = parser.parse_args()
     v = args.v[0]
     w = args.w[0]
+    label = args.l[0]
     case1Path = args.case1[0]
     v1 = args.v1[0]
     case2Path = args.case2[0]
@@ -61,14 +75,12 @@ def getArgs():
     yMax = args.r[1]
     
     saveFlag = args.s[0]
-    return (v,w,case1Path,v1,case2Path,v2,case3Path,v3,case4Path,v4,yMin,yMax,saveFlag)
+    return (v,w,case1Path,v1,case2Path,v2,case3Path,v3,case4Path,v4,yMin,yMax,label,saveFlag)
 
 def getFiles():
     timeFiles = os.listdir('postProcessing/patchAverage/')
     timeFiles = [f for f in timeFiles if f[0].isdigit()]
     timeFiles.sort()
-    #print(forceFiles)
-    #print(timeFiles)
     return timeFiles
 
 
@@ -76,8 +88,8 @@ def getData(v,w):
     timeFiles = getFiles()
     print('version =',v)
     
-    alphaV = []
-    timeV = []
+    alphaV = [] #np.array([])
+    timeV = [] #np.array([])
 
     for j,time in enumerate(timeFiles):
         files = os.listdir('postProcessing/patchAverage/'+time)
@@ -95,29 +107,50 @@ def getData(v,w):
                         timeV.append(time)
                         alphaV.append(alpha)
 
-    # grab alphaInlet
+    # grab alphaInlet, UInlet
     with open('caseGlobalVariables','r') as f:
         for line in f:
             if findWholeWord('alphaInlet')(line):
                 alphaInlet = getFloatFromString(line)
+            if findWholeWord('UInlet')(line):
+                UInlet = getFloatFromString(line)
+            if findWholeWord('rhow')(line):
+                rhoSlurry = getFloatFromString(line)
+            if findWholeWord('rhos')(line):
+                rhoSteam = getFloatFromString(line)
 
+    # grab density
     # compute carryover
-    alphaV = np.array(alphaV)/alphaInlet*100
+    alphaV = np.array(alphaV)
+    carryover = alphaV/alphaInlet*100
     indexStart = timeV.index(2)
-    print(np.average(alphaV[indexStart:]))
+    print('Average % Carryover',np.average(carryover[indexStart:]))
     
     # create rolling mean
     df = pd.DataFrame()
     df['Time'] = timeV
-    df['Alpha'] = alphaV
-    alphaMean = df.Alpha.rolling(w).mean()
+    df['Carryover'] = carryover
+    carryoverMean = df.Carryover.rolling(w).mean()
 
-    return timeV,alphaV,alphaMean
+    # calculate % wetness
+    os.system("surfaceCheck constant/triSurface/inlet.stl | grep 'Box' > boxI.txt")
+    with open('boxI.txt','r') as f:
+        for line in f:
+            DInlet = getDfromString(line)
+    areaInlet = DInlet**2*np.pi/4
+    totVol = UInlet*areaInlet
+    mSlurryOut = alphaV*totVol*rhoSlurry
+    mSteamOut = (1-alphaV)*totVol*rhoSteam
+    wetness = mSlurryOut/(mSlurryOut+mSteamOut)*100
+    print('Average % Wetness',np.average(wetness[indexStart:]))
 
-def saveData(timeV,power,c):
+
+    return timeV,carryover,carryoverMean
+
+def saveData(timeV,carryover,c):
     df = pd.DataFrame()
     df['Time'] = timeV
-    df['Alpha'] = alphaV
+    df['Carryover'] = carryover
     if c == 0:
         saveName = 'alphaCarryover.csv'
     else:
@@ -127,43 +160,43 @@ def saveData(timeV,power,c):
 
 
 #------- main code ------------------
-v,w,case1Path,v1,case2Path,v2,case3Path,v3,case4Path,v4,yMin,yMax,saveFlag = getArgs()
+v,w,case1Path,v1,case2Path,v2,case3Path,v3,case4Path,v4,yMin,yMax,label,saveFlag = getArgs()
 
 cwd = os.getcwd()
-timeV,alphaV,alphaMean = getData(v,w)
+timeV,carryover,carryoverMean = getData(v,w)
 caseCount = 1
 if case1Path != 'none':
     print('running case',case1Path)
     caseCount += 1
     os.chdir(case1Path)
-    time1V,alpha1,alpha1Mean = getData(v1,w)
+    time1V,carryover1,carryover1Mean = getData(v1,w)
 elif case2Path != 'none':
     print('running case',case2Path)
     caseCount += 1
     os.chdir(case2Path)
-    time2V,alpha2,alpha2Mean = getData(v2,w)
+    time2V,carryover2,carryover2Mean = getData(v2,w)
 elif case3Path != 'none':
     print('running case',case3Path)
     caseCount += 1
     os.chdir(case3Path)
-    time3V,alpha3,alpha3Mean = getData(v3,w)
+    time3V,carryover3,carryover3Mean = getData(v3,w)
 elif case4Path != 'none':
     print('running case',case4Path)
     caseCount += 1
     os.chdir(case4Path)
-    time4V,alpha4,alpha4Mean = getData(v4,w)
+    time4V,carryover4,carryover4Mean = getData(v4,w)
 
 os.chdir(cwd)
 if saveFlag:
-    saveData(timeV,power,0)
+    saveData(timeV,carryover,0)
     if caseCount > 1:
-        saveData(time1V,power1,1)
+        saveData(time1V,carryover1,1)
     elif caseCount > 2:
-        saveData(time2V,power2,2)
+        saveData(time2V,carryover2,2)
     elif caseCount > 3:
-        saveData(time3V,power3,3)
+        saveData(time3V,carryover3,3)
     elif caseCount > 4:
-        saveData(time4V,power4,4)
+        saveData(time4V,carryover4,4)
 
 
 
@@ -171,32 +204,33 @@ if saveFlag:
 
 if yMin == 'none':
     yMin = 0
-    yMax = max(alphaV)*1.1
+    yMax = max(carryover)*1.1
+if label == 'none':
+    label = 'Case 0'
 
 plt.figure(1,figsize=(8,6))
 font = {'family' : 'sans-serif',
         'weight' : 'normal',
         'size'   : 14}
 plt.rc('font',**font)
-plt.plot(timeV,alphaV,label='Case 0')
-plt.plot(timeV,alphaMean,'-k')
+plt.plot(timeV,carryover,label=label)
+plt.plot(timeV,carryoverMean,'-k',label='_nolegend_')
 if caseCount > 1:
-    plt.plot(time1V,alpha1,label='Case 1')
-    plt.plot(timeV,alpha1Mean)
+    plt.plot(time1V,carryover1)
+    plt.plot(timeV,carryover1Mean,label='_nolegend_')
 if caseCount > 2:
-    plt.plot(time2V,alpha2,label='Case 2')
-    plt.plot(timeV,alpha2Mean)
+    plt.plot(time2V,carryover2)
+    plt.plot(timeV,carryover2Mean,label='_nolegend_')
 if caseCount > 3:
-    plt.plot(time3V,alpha3,label='Case 3')
-    plt.plot(timeV,alpha3Mean)
+    plt.plot(time3V,carryover3)
+    plt.plot(timeV,carryover3Mean,label='_nolegend_')
 if caseCount > 4:
-    plt.plot(time4V,alpha4,label='Case 4')
-    plt.plot(timeV,alpha4Mean)
+    plt.plot(time4V,carryover4)
+    plt.plot(timeV,carryover4Mean,label='_nolegend_')
 
 plt.ylim(yMin,yMax)
 plt.grid()
 plt.xlabel('Simulation Time (s)')
-plt.ylabel('alpha')
-plt.legend()
+plt.ylabel('% Carryover')
+plt.legend(loc='upper left')
 plt.show()
-
