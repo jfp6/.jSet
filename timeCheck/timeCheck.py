48024506e09776eb4d09bc6e5c7d4683a3c76437
Joseph Prince <jprince@calderaengineering.com>
2022-01-18 13:16:14 -0700

    fix timeCheck

--- timeCheck/timeCheck.py
+++ timeCheck/timeCheck.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+from __future__ import division
 import re
 import numpy as np
 
@@ -14,18 +15,19 @@ def getLastFloatFromString(string,delimiter):
             num = (float(words[2]),float(words[7]))
         except:
             num = (float(words[2]),float(words[2]))
+    elif words[0] == 'deltaT':
+        num = float(words[-1])
     else:
-        if words[-1][-1] =='s':
-            num =float(words[-1][:-2])
+        if words[-1][-2] =='s':
+            num =float(words[-1][:-3])
         else:
             num = float(words[-1])
     return num
 
 
-
 # --------------- code begins ------------------
 
-
+# Get endTime
 with open('system/controlDict','r') as file:
     for i,line in enumerate(file):
         if line[:7] == 'endTime':
@@ -34,50 +36,46 @@ with open('system/controlDict','r') as file:
             except:
                 endTime = getLastFloatFromString(line,'\t')
 
+# Initiallize arrays
 deltaT = np.array([])
 realTime = np.array([])
 
+# Get other times
 with open('timeCheck.txt','r') as file:
     for i,line in enumerate(file):
         if findWholeWord('deltaT')(line):
-            deltaT = np.append(deltaT,getLastFloatFromString(line,'='))
+            deltaT = np.append(deltaT,getLastFloatFromString(line,' '))
         if findWholeWord('ExecutionTime')(line):
             num = getLastFloatFromString(line,' ')
             realTime = np.append(realTime,num[0])
             clockTime = num[1]
-        if line[:4] == 'Time':
+        if findWholeWord('Time')(line):
             try:
-                currentTime = getLastFloatFromString(line,'=')
+                currentTime = getLastFloatFromString(line,' ')
             except:
                 print('Preliminary')
         if findWholeWord('Date')(line):
             deltaT = np.array([])
             realTime = np.array([])
 
-# get average simulation time step size
+# Get average simulation time step size
 deltaTavg = np.average(deltaT)
 
-# get average realTime it takes for each timeStep
+# Get average realTime it takes for each timeStep
 realDeltaT = np.diff(realTime)
 realDeltaTavg = np.average(realDeltaT)
 
-#print(realDeltaT)
-#print(realDeltaTavg)
-#print(clockDeltaT)
-#print(clockDeltaTavg)
-
-# calculate how long it will take
+# Calculate how long it will take
 timeLeft = endTime - currentTime
 simTime = np.array([timeLeft,1,10,100])
-
 f = realTime[-1]/clockTime
-
 runTime = simTime*realDeltaTavg/deltaTavg/3600/f
 
-# print results
-print('Currently at',currentTime,'EndTime is ',endTime)
+# Print results
+print('Currently at',currentTime,', EndTime is ',endTime)
 print('deltaT is ' + str(round(deltaTavg,6)) + ' s in ' + str(round(realDeltaTavg,2)) + ' s') 
 print('Time to finish simulation',round(runTime[0],1),'hours,',round(runTime[0]/24,1),'days')
 print('Time to run 1 sec',round(runTime[1],1),'hours,',round(runTime[1]/24,1),'days')
 print('Time to run 10 sec',round(runTime[2],1),'hours,',round(runTime[2]/24,1),'days')
 print('Time to run 100 sec',round(runTime[3],1),'hours,',round(runTime[3]/24,1),'days')
+
